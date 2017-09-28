9dec3c5a17225f26a8f1186b91d3fc47ebd9ff50
Saix_93 <r.torresgonzalez.93@gmail.com>
2017-09-28 17:59:18 +0200

    Añade los cambios realizados hoy en clase

---
+++ Assets/Procedural/CirclePoints.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CirclePoints : MonoBehaviour
+{
+    [SerializeField] [Range(1, 20)] int _points = 10;
+    [SerializeField] [Range(1, 10)] float _interiorScale = 1;
+    [SerializeField] [Range(1, 10)] float _exteriorScale = 1;
+    float _increment;
+
+    private void Start()
+    {
+        _increment = Mathf.PI * 2 / _points;
+
+        for (int i = 0; i < _points + 1; i++)
+        {
+            float f = _increment * i;
+            float x = Mathf.Sin(f);
+            float y = Mathf.Cos(f);
+
+            Vector3 interiorPosition = new Vector3(x, 0, y) * _interiorScale;
+            Vector3 exteriorPosition = new Vector3(x, 0, y) * _exteriorScale;
+
+            CreateSphere(interiorPosition, i);
+            CreateSphere(exteriorPosition, i);
+        }
+    }
+
+    GameObject CreateSphere(Vector3 position, int index = -1)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+
+        sphere.transform.localScale = Vector3.one * .1f;
+        sphere.transform.position = position;
+
+        if (index >= 0)
+        {
+            GameObject textGO = new GameObject("text");
+            TextMesh text = textGO.AddComponent<TextMesh>();
+            text.anchor = TextAnchor.MiddleCenter;
+            text.text = index.ToString();
+            text.color = Color.red;
+            text.characterSize = .1f;
+
+            textGO.transform.SetParent(sphere.transform);
+            textGO.transform.rotation = Quaternion.Euler(90, 0, 0);
+            textGO.transform.localPosition = Vector3.zero;
+        }
+
+        return sphere;
+    }
+}
---
+++ Assets/Roads/RoadPortion.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadPortion
+{
+    /// <summary>
+    /// La forma que va a tener este trozo de geometría
+    /// </summary>
+    public enum EShape
+    {
+        Line,
+        RightCurve,
+        LeftCurve
+    }
+
+    readonly float WIDTH_ROAD;
+    readonly float LONG_ROAD;
+    readonly int SUBDIVISION_CURVE;
+    readonly int SUBDIVISION_LINE;
+
+    public RoadPortion(float widthRoad, float longRoad, int subdivisionCurve, int subdivisionLine)
+    {
+        WIDTH_ROAD = widthRoad;
+        LONG_ROAD = longRoad;
+        SUBDIVISION_CURVE = subdivisionCurve;
+        SUBDIVISION_LINE = subdivisionLine;
+    }
+
+    Mesh GetMesh(EShape shape)
+    {
+        Mesh mesh = new Mesh();
+        List<Vector3> vertices = new List<Vector3>();
+        List<Vector3> normals = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+        List<int> triangles = new List<int>();
+
+        mesh.SetVertices(vertices);
+        mesh.triangles = triangles.ToArray();
+        mesh.normals = normals.ToArray();
+        mesh.uv = uvs.ToArray();
+
+        return mesh;
+    }
+
+    void GenerateLine(List<Vector3> vertices, List<Vector3> normals, List<Vector2> uvs, List<int> triangles)
+    {
+
+    }
+
+    void GenerateCurve(int direction, List<Vector3> vertices, List<Vector3> normals, List<Vector2> uvs, List<int> triangles)
+    {
+
+    }
+}
